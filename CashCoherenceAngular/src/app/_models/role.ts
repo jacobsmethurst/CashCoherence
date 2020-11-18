adbd1923d74264e0ed42af6a26f75f9e98113446
jcsmeth <jcsmeth@vt.edu>
2020-11-17 23:02:13 -0500

    reset everything

--- CashCoherenceAngular/src/app/_models/role.ts
+++
@@ -1,4 +0,0 @@
-export enum Role {
-    student = 'Student',
-    professor = 'Professor'
-}
--- CashCoherenceAngular/src/app/_models/user.ts
+++
@@ -1,8 +0,0 @@
-import { Role } from './role';
-
-export class User {
-    username: string;
-    password: string;
-    role: Role;
-    token?: string;
-   }
