31547728f4e3df7d5cdb0f84930a71feb38bbe8e
jcsmeth <jcsmeth@vt.edu>
2020-11-19 17:53:47 -0500

    beginning of basic table view

--- ccangular/src/app/material.module.ts
+++ ccangular/src/app/material.module.ts
@@ -6,6 +6,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
     exports: [
@@ -15,7 +17,9 @@ import { MatButtonModule } from '@angular/material/button';
         MatCardModule,
         MatFormFieldModule,
         MatInputModule,
-        MatButtonModule
+        MatButtonModule,
+        MatTableModule,
+        MatSortModule
     ]
 })
 export class MaterialModule {}
--- ccangular/src/app/transactions/transactions.component.ts
+++ ccangular/src/app/transactions/transactions.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
   styleUrls: ['./transactions.component.css']
 })
-export class TransactionsComponent implements OnInit {
+export class TransactionsComponent implements AfterViewInit {
+
+  testData = [
+    {
+      name: 'test',
+      date: new Date().toDateString(),
+      amount: 50.0,
+      category: 'testcat'
+    },
+    {
+      name: 'test2',
+      date: new Date().toDateString(),
+      amount: -55.0,
+      category: 'newcat'
+    }
+  ];
+
+  displayedColumns: string[] = ['date', 'name', 'amount', 'category', 'actions'];
+  dataSource = new MatTableDataSource(this.testData);
+
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
+
+  getAbs(i: number): number {
+    return Math.abs(i);
   }
 
 }
